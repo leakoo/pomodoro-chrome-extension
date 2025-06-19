860591ef7b32caf6c75c47c95cebf1d2f714280e
Dan <139128814+leakoo@users.noreply.github.com>
2025-06-19 14:57:11 +0100

    chore: Install viteStaticCopy for background.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/background/background.js',
+          dest: ''
+        }
+      ]
+    })
   ],
-})
+  build: {
+    outDir: 'dist'
+  }
+});
