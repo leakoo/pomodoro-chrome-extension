0e1153e57599d9e11cf72b44becaa20232fc8b52
Dan <139128814+leakoo@users.noreply.github.com>
2025-06-26 13:27:48 +0100

    feat: Implement autoStart state and get auto state from background.js

--- src/app.jsx
+++ src/app.jsx
@@ -7,15 +7,17 @@ export default function App() {
   const [timeLeft, setTimeLeft] = useState(null);
   const [isRunning, setIsRunning] = useState(false);
   const [mode, setMode] = useState("work");
+  const [autoStart, setAutoStart] = useState(false);
 
   // Get updated state from background.js every 100ms to keep everything in sync
   useEffect(() => {
     const interval = setInterval (() => {
-      chrome.storage.local.get(["timeLeft", "isRunning", "mode"], (res) => {
+      chrome.storage.local.get(["timeLeft", "isRunning", "mode", "auto"], (res) => {
         // If result is not undefined update state
         if (res.timeLeft !== undefined) setTimeLeft(res.timeLeft);
         if (res.isRunning !== undefined) setIsRunning(res.isRunning);
         if (res.mode !== undefined) setMode(res.mode);
+        if (res.auto !== undefined) setAutoStart(res.auto);
       });
     }, 100);
 
