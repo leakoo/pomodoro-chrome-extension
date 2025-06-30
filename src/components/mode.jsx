cc4f5e4ddab1b42f25b307ee2ef4a66d40ed06f9
Dan <139128814+leakoo@users.noreply.github.com>
2025-06-30 13:18:51 +0100

    feat: Implement random emojis for when mode changes

--- src/components/mode.jsx
+++ src/components/mode.jsx
@@ -1,8 +1,24 @@
+import { useMemo } from "react";
+
 export default function Mode({ mode }) {
-    
+
+  const emoji = useMemo(() => {
+    const workEmojis = ["🧠", "💻", "✍️", "📝", "🔥", "🎯"];
+    const breakEmojis = ["☕", "🧘", "🌿", "🚶", "🎵", "🛋️"];
+
+    if (mode === "work") {
+      return workEmojis[Math.floor(Math.random() * workEmojis.length)]
+    }
+    else {
+      return breakEmojis[Math.floor(Math.random() * breakEmojis.length)]
+    }
+
+  }, [mode])
+
   return (
     <>
       <h1>{mode}</h1>
+      <h2>{emoji}</h2>
     </>
   );
 };
