1ce06c004c4ed83d216f2e5f41a79ee135f7be8a
Dan <139128814+leakoo@users.noreply.github.com>
2025-07-02 15:36:50 +0100

    feat: Add message listener to play audio alarm

--- src/offscreen/offscreen.js
+++ src/offscreen/offscreen.js
@@ -1,2 +1,14 @@
-const audio = document.querySelector(".audio");
-
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  if (message.action === "PLAY_SOUND") {
+    try {
+      const audio = new Audio(chrome.runtime.getURL("assets/mixkit-digital-quick-tone-2866.mp3"));
+      await audio.play();
+      sendResponse({ status: "sound played" });
+    }
+    catch(error) {
+      console.error("Audio failed to play", error);
+      sendResponse({ status: "error", error: error.message });
+    }
+    return true;
+  }
+});
