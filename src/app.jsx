98770260f1d11564c7701970a22da4cb1aba63fe
Dan <139128814+leakoo@users.noreply.github.com>
2025-07-22 16:24:20 +0100

    feat: Add theme state, sync with storage, update DOM class and render DarkMode

--- src/app.jsx
+++ src/app.jsx
@@ -2,41 +2,58 @@ import { useState, useEffect } from "react";
 import TimerControls from "./components/timer-controls.jsx";
 import TimerDisplay from "./components/timer-display.jsx";
 import Mode from "./components/mode.jsx";
+import DarkMode from "./components/dark-mode.jsx";
 
 export default function App() {
   const [timeLeft, setTimeLeft] = useState(null);
   const [isRunning, setIsRunning] = useState(false);
   const [mode, setMode] = useState("Work");
   const [autoStart, setAutoStart] = useState(false);
+  const [theme, setTheme] = useState("light");
 
   // Get updated state from background.js every 100ms to keep everything in sync
   useEffect(() => {
     const interval = setInterval (() => {
-      chrome.storage.local.get(["timeLeft", "isRunning", "mode", "auto"], (res) => {
+      chrome.storage.local.get(["timeLeft", "isRunning", "mode", "auto", "theme"], (res) => {
         // If result is not undefined update state
         setTimeLeft(res.timeLeft ?? 10);
         setIsRunning(res.isRunning ?? false);
         setMode(res.mode ?? "Work");        
         setAutoStart(res.auto ?? false);
+        setTheme(res.theme ?? "light");
       });
     }, 100);
 
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <>
-      <TimerDisplay
-        timeLeft={timeLeft}
-      />
+  useEffect(() => {
+    const htmlTag = document.getElementById("darkmode");
 
-      <Mode
-        mode={mode}
-      />
+    if (theme === "dark") {
+      htmlTag.classList.add("dark");
+    }
+    else htmlTag.classList.remove("dark");
+
+  }, [theme]);
+
+  return (
+    <div className="flex justify-center items-center bg-[#EEE] dark:bg-[#111] p-6 min-h-screen text-[#111] dark:text-[#EEE]">
+      <div className="p-2 w-full">
+        <DarkMode />
+
+        <TimerDisplay 
+          timeLeft={timeLeft} 
+        />
+
+        <Mode 
+         mode={mode} 
+        />
       
-      <TimerControls
-        autoStart={autoStart} 
-      />
-    </>
+        <TimerControls 
+         autoStart={autoStart} 
+        />
+      </div>
+    </div>
   )
 };
