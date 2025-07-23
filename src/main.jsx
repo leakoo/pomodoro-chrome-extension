70f2d7a8d7df1484c921d996861cae98abbda2a5
Dan <139128814+leakoo@users.noreply.github.com>
2025-07-23 13:28:31 +0100

    feat: Implement initialData with storage.local to load data before React mounts

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app.jsx';
 
-createRoot(document.getElementById('root')).render(
+chrome.storage.local.get(["timeLeft", "isRunning", "mode", "auto", "theme"], (res) => {
+  const initialData  = {
+    timeLeft: res.timeLeft ?? 25 * 60,
+    isRunning: res.isRunning ?? false,
+    mode: res.mode ?? "Work",
+    autoStart: res.auto ?? false,
+    theme: res.theme ?? "light"
+  }
+
+  if (initialData.theme === "dark") {
+    document.getElementById("darkmode").classList.add("dark");
+  };
+
+  createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App initialData ={initialData } />
   </StrictMode>,
-);
+  );
+});
