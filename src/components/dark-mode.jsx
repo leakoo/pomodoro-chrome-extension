d25b1cd993370d8cbbadc96e52c6802bcd109acf
Dan <139128814+leakoo@users.noreply.github.com>
2025-08-28 15:49:14 +0100

    style: Add hover effect, change icon size and remove duplicate style

--- src/components/dark-mode.jsx
+++ src/components/dark-mode.jsx
@@ -11,10 +11,10 @@ export default function DarkMode({ theme }) {
 
   return (
     <>
-      <button onClick={toggleDarkMode} className="">
+      <button onClick={toggleDarkMode} className="top-4 left-4 absolute hover:transition-transform duration-150 cursor-pointer ease">
         {theme === "light" ?
-          <img width="25" src="./assets/moon.png" className="top-4 left-4 absolute rotate-20 hover:-rotate-20 hover:transition-transform duration-150 cursor-pointer transistion ease" alt="Moon icon" /> :
-          <img width="25" src="./assets/sun.png" className="top-4 left-4 absolute hover:rotate-45 hover:transition-transform duration-150 cursor-pointer transistion ease" alt="Sun icon" />
+          <img width="33" src="./assets/moon.png" className="hover:bg-gray-400/80 p-1 rounded-full rotate-20 hover:-rotate-20 transition" alt="Moon icon" /> :
+          <img width="33" src="./assets/sun.png" className="hover:bg-gray-700/80 p-1 rounded-full hover:rotate-45 transition" alt="Sun icon" />
         }
       </button>
     </>
